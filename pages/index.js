7a32ac01bfc0e270743dde8103b4573ef24caa59
SuanSilva <suansilva20@gmail.com>
2021-07-15 21:37:21 -0300

    home e add do profileRelations

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/componentes/MainGrid'
+import Box from '../src/componentes/Box'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/componentes/ProfileRelations/ProfileRelationsBoxWrapper';
+
+function ProfileSidebar(propriedades) {
+    return (
+      <Box>
+        <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px'}}/>
+      </Box>
+    )
+  }
 
 export default function Home() {
-  return <Title>My page</Title>
-}
+    const githubUser= 'SuanSilva';
+    const pessoasFavoritas= [
+        'juunegreiros',
+        'omariosouto',
+        'peas',
+        'rafaballerini',
+        'marcobrunodev',
+        'felipefialho'
+    ]
+    return (
+        <>
+        <AlurakutMenu/>
+        <MainGrid> 
+    { /* <Box style="grid-area: profileArea;"> */}     
+        <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+            <ProfileSidebar githubUser={githubUser} /> 
+        </div>
+        <div className="welcomeArea"style={{gridArea:'welcomeArea'}}>
+            <Box>
+                Bem vindo 
+            </Box>
+        </div>
+        <div className="profileRelationsArea"style={{gridArea:'profileRelationsArearea'}}>
+            <ProfileRelationsBoxWrapper>
+                <h2 className="smallTitle">
+                Amigos
+                </h2>
+                
+            <ul>       
+                {pessoasFavoritas.map((itemAtual)=>{
+                    return(
+                        <a href={`/users/${itemAtual}`} key={itemAtual}>
+                        <img src={`https://github.com/${itemAtual}.png`}/>
+                        <span>{itemAtual}</span>  
+                        </a>   
+                    )
+                })}
+            </ul>  
+            </ProfileRelationsBoxWrapper>          
+            <Box>
+                Comunidades
+            </Box>
+        </div>    
+        </MainGrid>
+        </> 
+        )
+        }
+
+
+
+
