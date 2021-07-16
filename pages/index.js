035b5804ee82f097cf67c9aa758a711b4b5908ab
SuanSilva <suansilva20@gmail.com>
2021-07-15 23:41:59 -0300

    add form and icons to profilebox

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,19 @@ import { ProfileRelationsBoxWrapper } from '../src/componentes/ProfileRelations/
 
 function ProfileSidebar(propriedades) {
     return (
-      <Box>
+      <Box as="aside">
         <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px'}}/>
+        <hr/>
+       
+        <p>
+          <a className="boxLink"href={`https://github.com/${propriedades.githubUser}`}>
+             @{propriedades.githubUser}
+          </a>
+        </p>
+        
+        <hr/>
+        
+        <AlurakutProfileSidebarMenuDefault/>
       </Box>
     )
   }
@@ -34,6 +45,48 @@ export default function Home() {
             <Box>
                 <h1 ClassName="title">Bem vindo(a)</h1> 
                 <OrkutNostalgicIconSet/>
+            </Box>
+
+            <Box>
+            <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={function handleCriaComunidade(e) {
+                e.preventDefault();
+                const dadosDoForm = new FormData(e.target);
+
+                console.log('Campo: ', dadosDoForm.get('title'));
+                console.log('Campo: ', dadosDoForm.get('image'));
+
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get('title'),
+                  image: dadosDoForm.get('image'),
+                }
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+                setComunidades(comunidadesAtualizadas)
+            }}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                  />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+
+              <button>
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
+            
             </Box>
         </div>
         <div className="profileRelationsArea"style={{gridArea:'profileRelationsArea'}}>
